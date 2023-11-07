ab3d944b4bec08e695ccb8227eb2f9fde075414c
Oleg <kovalyovop@yandex.ru>
2023-11-08 02:10:20 +0300

    Start

--- src/main/java/org/example/Duck.java
+++ src/main/java/org/example/Duck.java
@@ -1,8 +1,10 @@
 package org.example;
 
-public class Duck implements FlyBehavior{
-    @Override
-    public void fly() {
-        System.out.println("Полет");
+public abstract class Duck {
+    QuackBehavior quackBehavior;
+
+    public void performQuack() {
+        quackBehavior.quack();
     }
+
 }
