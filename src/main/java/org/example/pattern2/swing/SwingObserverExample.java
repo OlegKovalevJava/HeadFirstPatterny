a41fb74f953dfa39cae53b90105c8be23597e5c2
Oleg <kovalyovop@yandex.ru>
2024-01-07 13:26:44 +0300

    pattern2 +++++

---
+++ src/main/java/org/example/pattern2/swing/SwingObserverExample.java
@@ -0,0 +1,19 @@
+package org.example.pattern2.swing;
+
+import javax.swing.*;
+
+public class SwingObserverExample {
+    JFrame frame;
+
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(event -> System.out.println("Don't do it, you might regret it!"));
+        button.addActionListener(event -> System.out.println("Come on, do it!"));
+    }
+}
