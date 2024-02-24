d1d681236c08ac7fe5e26455b50ae61451754c86
Oleg <kovalyovop@yandex.ru>
2024-02-24 13:55:12 +0300

    7

--- src/main/java/org/example/pattern6COMMAND/two/DatabaseRunner.java
+++ src/main/java/org/example/pattern6COMMAND/two/DatabaseRunner.java
@@ -2,18 +2,6 @@ package org.example.pattern6COMMAND.two;
 
 public class DatabaseRunner {
     public static void main(String[] args) {
-        Database dataBase = new Database();
 
-        Developer developer = new Developer(
-                new InsertCommand(dataBase),
-                new UpdateCommand(dataBase),
-                new SelectCommand(dataBase),
-                new DeleteCommand(dataBase)
-        );
-
-        developer.insertRecord();
-        developer.updateRecord();
-        developer.selectRecord();
-        developer.deleteRecord();
     }
 }
